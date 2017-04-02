9099b04dfabb63e9d655c75553a56aef2892f850
hotchner <1178956984@qq.com>
2017-04-02 18:07:38 +0800

    Initial Commit

---
+++ HuaWeiOJ/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HuaWeiOJ
+//
+//  Created by hotch on 2017/4/2.
+//  Copyright © 2017年 hotch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
