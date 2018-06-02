a0f157e2f252f5502ea72b521dcfe43042dd4686
hongqima <hongqima@yangqianguan.com>
2018-06-02 18:03:20 +0800

    leetCode两数之和

---
+++ twoSum.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+  for i in 0..<nums.count - 1 {
+    for j in i + 1..<nums.count {
+      if nums[i] + nums[j] == target {
+        return [i, j]
+      }
+    }
+  }
+  return []
+}
