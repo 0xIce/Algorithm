942594bcfde73b4bcb2a2fc79fbb7d1c1566c4dc
hongqima <hongqima@yangqianguan.com>
2018-06-13 19:33:10 +0800

    调整目录结构

--- twoSum/twoSum.playground/Contents.swift
+++ twoSum/twoSum.playground/Contents.swift
@@ -11,15 +11,4 @@
  所以返回 [0, 1]
  */
 
-import Cocoa
 
-func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-  for i in 0..<nums.count - 1 {
-    for j in i + 1..<nums.count {
-      if nums[i] + nums[j] == target {
-        return [i, j]
-      }
-    }
-  }
-  return []
-}
