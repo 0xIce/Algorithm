c796fee8f78360b4bcd85db4f4d28ebdeb837d0a
hongqima <hongqima@yangqianguan.com>
2018-06-15 23:37:17 +0800

    Dijkstra-双栈算法

--- ArithmeticExpressionEvaluation/ArithmeticExpressionEvaluation.playground/Contents.swift
+++ ArithmeticExpressionEvaluation/ArithmeticExpressionEvaluation.playground/Contents.swift
@@ -1,15 +1,1 @@
-func evaluate(_ expression: String) -> Int {
-  var oprations = Stack<String>()
-  var values = Stack<Double>()
-  expression.forEach { (char) in
-    switch char {
-    case "+", "-":
-      print(char)
-    default:
-      break
-    }
-  }
-  return -1
-}
-
-evaluate("1 * (2 + 3)")
+evaluate("(4 * (3 + 8))")
--- ArithmeticExpressionEvaluation/ArithmeticExpressionEvaluation.playground/Sources/ArithmeticExpressionEvaluation.swift
+++ ArithmeticExpressionEvaluation/ArithmeticExpressionEvaluation.playground/Sources/ArithmeticExpressionEvaluation.swift
@@ -1,9 +1,40 @@
-import Cocoa
+/// Dijkstra-双栈算法 计算算术表达式的值，没有优先级，只能通过小括号判断计算顺序
+var oprations = Stack<String>()
+var values = Stack<Double>()
+public func evaluate(_ expression: String) -> Double {
+  expression.forEach { (char) in
+    switch char {
+    case "+", "-", "*", "/":
+      oprations.push(char.description)
+    case "0"..."9":
+      values.push(Double(char.description)!)
+    case ")":
+      values.push(performOperation())
+    default:
+      break
+    }
+  }
+  return values.pop() ?? -1
+  
+}
 
-/// 计算算术表达式的值，没有优先级，只能通过小括号判断计算顺序
-//public func evaluate(_ expression: String) -> Int {
-//  var oprations = Stack<String>()
-//  var values = Stack<Double>()
-//  return -1
-//}
+private func performOperation() -> Double {
+  guard let op = oprations.pop(),
+    let value1 = values.pop(),
+    let value2 = values.pop() else {
+      return -1
+  }
+  switch op {
+  case "+":
+    return value2 + value1
+  case "-":
+    return value2 + value1
+  case "*":
+    return value2 * value1
+  case "/":
+    return value2 / value1
+  default:
+    return -1
+  }
+}
 
