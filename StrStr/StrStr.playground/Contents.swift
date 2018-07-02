14efedf94529291a512b8b354efca0e5e3d7746d
hongqima <hongqima@yangqianguan.com>
2018-07-03 01:50:16 +0800

    添加strStr

---
+++ StrStr/StrStr.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+public func strStr(_ haystack: String, _ needle: String) -> Int {
+  guard !needle.isEmpty else {
+    return 0
+  }
+  for searchIndex in needle.indices {
+    
+  }
+  return -1
+}
+
+
+//: test
+strStr("testt", "tt")
